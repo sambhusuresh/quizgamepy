b8d1d85465849683da88c134fb6886ea34907dc8
Sambhu_suresh <50201955+sambhusuresh@users.noreply.github.com>
2022-07-31 19:01:45 +0530

    fun project for beginners

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Welcome To the Tech Quiz")
 playing  = input("do you want to play? ")
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 print("Okay! Let's play :) ")
 score = 0 
@@ -12,3 +12,15 @@ else:
     print('incorrect')
     score+= -1
     #negetive marking
+
+answer = input("full form of cpu? ")
+if answer.lower() == "central proccesing unit":
+    print('correct!')
+    score+= 4
+else:
+    print('incorrect')
+    score+= -1
+#i am so lazy to add more question becouse its not thrilling 
+
+#mark calculation
+print("your mark is " + str(score/4) + " out of 2")
