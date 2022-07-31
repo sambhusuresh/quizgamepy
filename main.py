26f975cde3156404405a97e79b5143987526bf30
Sambhu_suresh <50201955+sambhusuresh@users.noreply.github.com>
2022-07-31 18:52:23 +0530

    introducing negetive marking

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Welcome To the Tech Quiz")
+playing  = input("do you want to play? ")
+if playing != "yes":
+    quit()
+print("Okay! Let's play :) ")
+score = 0 
+answer = input("full form of RAM? ")
+if answer.lower() == "random acces memmory":
+    print('correct!')
+    score+= 4
+else:
+    print('incorrect')
+    score+= -1
+    #negetive marking
